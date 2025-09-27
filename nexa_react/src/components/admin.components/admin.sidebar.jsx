b1836caa16f89b6b518dc1d263e2e1298e54f59b
Ehara Perera <eharaperera@gmail.com>
2025-09-27 11:28:37 +0530

    fix 1.1

--- nexa_react/src/components/admin.components/admin.sidebar.jsx
+++ nexa_react/src/components/admin.components/admin.sidebar.jsx
@@ -1,7 +1,6 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../../assets/global.assets/logo3.png";
 
-
 export default function AdminSidebar() {
 
   const navigate = useNavigate();
