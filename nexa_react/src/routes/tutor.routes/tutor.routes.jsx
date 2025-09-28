6bb7825d6f8ca6b26a7d81c81d1a5275eea137e6
Eric Devon <ericdevon2003@gmail.com>
2025-09-28 10:25:10 +0530

    Tutor Dashboard and Profile update

--- nexa_react/src/routes/tutor.routes/tutor.routes.jsx
+++ nexa_react/src/routes/tutor.routes/tutor.routes.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import TutorPage from '../../pages/tutor.pages/tutor.page'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import TutorPage from "../../pages/tutor.pages/tutor.page";
+import TutorProfile from "../../pages/tutor.pages/tutor.profile.page";
+import TutorDashboard from "../../pages/tutor.pages/tutor.dashboard.page";
 
 function TutorRoutes() {
-    return (
-        <Routes>
-            <Route path='/' element={<TutorPage />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      {/* Group 1: Routes WITH top navbar (using TutorPage as layout) */}
+      <Route path="/" element={<TutorPage />}>
+        <Route index element={<TutorDashboard />} />
+        <Route path="courses" element={<div>My Courses Page</div>} />
+        <Route path="schedule" element={<div>Schedule Page</div>} />
+        <Route path="assignments" element={<div>Assignments Page</div>} />
+        <Route path="students" element={<div>Students Page</div>} />
+        <Route path="analytics" element={<div>Analytics Page</div>} />
+        <Route path="profile" element={<TutorProfile />} />
+        <Route path="settings" element={<div>Settings Page</div>} />
+        <Route path="earnings" element={<div>Settings Page</div>} />
+        <Route path="help" element={<div>Settings Page</div>} />
+      </Route>
+
+      {/* Group 2: Routes WITHOUT top navbar (standalone pages) */}
+      {/* <Route path="/profile" element={<TutorProfile />} /> */}
+    </Routes>
+  );
 }
 
-export default TutorRoutes
+export default TutorRoutes;
